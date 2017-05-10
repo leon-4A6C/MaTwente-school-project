f02ffa175d794e808ce32020de657cc92a0d1da5
leon3110l <leon3110l@gmail.com>
2017-05-10 22:12:55 +0200

    fixed some stuff

--- src/incidenten-overzicht.php
+++ src/incidenten-overzicht.php
@@ -122,8 +122,7 @@ include "functions.php";
         dataToDb("83.82.240.2", "user", "pass", "project", "incidenten", "DELETE FROM incidenten WHERE id = ".$_POST["delete-id"]);
       }
       $incidenten_data = sqlSelect("83.82.240.2", "user", "pass", "project", "SELECT voornaam,	achternaam, id,	afhandel_tijd	up_votes,	onderwerp	omschrijving,	gebruikers_id,	aanmelddag,	verantwoordelijke_id,	oorzaak,	oplossing	terugkoppeling,
-        FROM incidenten , gebruikers as g
-        WHERE  incidenten.verantwoordelijke_id = g.id )";
+        FROM incidenten , gebruikers as g WHERE incidenten.verantwoordelijke_id = g.id");
       if ($_SESSION["user"]["toegangs_level"] == "admin") {
         foreach ($incidenten_data as $key => $value) {
           $forms = "<form id='".$value['id']."-edit' action='change-ticket.php' method='post' style='display:none'>";
